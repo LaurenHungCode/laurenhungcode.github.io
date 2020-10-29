525f6355148e8dfce3ec06fa1c9d89247f69acec
LaurenHung <hsienhuh@andrew.cmu.edu>
2020-10-29 15:19:52 -0400

    js update

--- homework_6/main.js
+++ homework_6/main.js
@@ -1,6 +1,34 @@
+
+var productArr = []
+
+//This is going to be a product class for buns you can purchase
 class Product {
-    constructor (quantity, glaze) {
-        this.quantity = quantity
-        this.glaze = glaze
+	constructor(quant, glaze) {
+		this.quant = quant
+		this.glaze = glaze
+	}
+}
+
+function addToCart() {
+	//alert('add to cart')
+    
+    // glaze
+    var glaze = document.getElementById('form').value
+
+    // guant
+    var quant = document.getElementById('quant').value
+
+	var quantCount = parseInt(quant)
+	for(var i = 0; i < quantCount; i++) {
+		var bun = new Product(quant, glaze)
+		productArr.push(bun)	
     }
+    console.log(productArr)
+	
+	updateCartNumber(productArr.length)
+}
+
+function updateCartNumber(num) {
+	var cartCount = document.getElementById('cartCount')
+	cartCount.innerHTML = num
 }
